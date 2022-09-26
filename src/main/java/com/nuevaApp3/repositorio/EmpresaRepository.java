57e3f29ab699a3cc2b52a3cd81ac43b45a5729be
Cecilio José Avila Ramos <110960338+CeciliojoseAvila@users.noreply.github.com>
2022-09-26 10:54:05 -0500

    Initial commit

---
+++ src/main/java/com/nuevaApp3/repositorio/EmpresaRepository.java
@@ -0,0 +1,9 @@
+package com.nuevaApp3.repositorio;
+
+import com.nuevaApp3.modelos.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository //Anotacion que le dice a Spring que esta clase es un repositorio
+public interface EmpresaRepository extends JpaRepository<Empresa, Integer> {
+}
